dd1c08d41d40e44b1c0d59d8e713fa7aff0d9764
amosaugusto <amosaugusto@gmail.com>
2022-05-13 10:25:48 +0700

    repositories and services

---
+++ app/repositories/carRepository.js
@@ -0,0 +1,52 @@
+const { Car } = require("../models");
+const { Op, DATE } = require("sequelize");
+const res = require("express/lib/response");
+
+module.exports = {
+  getAll() {
+    return Car.findAll({
+      where: {
+        deleted_by: {
+          [Op.eq]: null,
+        },
+      },
+    });
+  },
+  create(car) {
+    return Car.create({
+      name: car.name,
+      created_by: car.createdBy,
+      deleted_by: null,
+      last_updated_by: car.createdBy,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+  },
+  updateById(car) {
+    return Car.findOne({
+      where: {
+        id: car.id,
+      },
+    }).then((updatedCar) => {
+      if (!updatedCar) return res.status(404).send();
+      updatedCar.update({
+        name: car.name,
+        last_updated_by: car.updatedBy,
+        updatedAt: new Date(),
+      });
+    });
+  },
+  deleteById(car) {
+    return Car.findOne({
+      where: {
+        id: car.id,
+      },
+    }).then((deletedCar) => {
+      if (!deletedCar) return res.status(404).send();
+      deletedCar.update({
+        deleted_by: car.deletedBy,
+        updatedAt: new Date(),
+      });
+    });
+  },
+};
